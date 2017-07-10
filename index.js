2e661326ef5d8b4aaf1812402e20a07092aa9134
Ben Cook <benthomascook28@gmail.com>
2017-07-10 16:06:55 -0400

    setup database

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const http = require('http');
 const bodyParser = require('body-parser');
 const router = require('./router');
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:bucket/bucket');
 
 app.use(bodyParser.json({type: '*/*'}))
 router(app);
---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+	email: {
+		type: String,
+		unique: true,
+		lowercase: true
+	},
+	password: String
+});
+
+let model = mongoose.model('user', userSchema)
+
+module.export = model;
