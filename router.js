39c1affa25cafdf5afbd9a78bcba18d5ef9be2aa
Ben Cook <benthomascook28@gmail.com>
2017-07-11 13:24:39 -0400

    added passport authentication

--- router.js
+++ router.js
@@ -1,6 +1,15 @@
 const Auth = require('./controllers/auth');
-const User = require('./models/user');
+var passportService = require('./services/passport');
+var passport = require('passport');
+
+var requireAuth = passport.authenticate('jwt', {session: false});
 
 module.exports = function(app){
+
+	app.get('/', requireAuth, function(req, res){
+		res.send('Hello Homepage');
+		//res.send({hi: 'there'});
+	});
+	
 	app.post('/signup', Auth.signup);
 }
