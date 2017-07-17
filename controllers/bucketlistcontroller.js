619eb5c135a6cde84209d35668631833922877b1
Ben Cook <benthomascook28@gmail.com>
2017-07-16 20:36:17 -0400

    Added module 24 client fetchall

--- controllers/bucketlistcontroller.js
+++ controllers/bucketlistcontroller.js
@@ -1,18 +1,20 @@
 var BucketList = require('../models/bucketlist.js');
 
 exports.addBucketList = function(req, res, next){
+	console.log("Yay, we're inside of BucketList")
 	//for Postman use
-	// var title = req.body.title;
-	// var topic = req.body.topic;
-	// var url = req.body.url;
-	// var content = req.body.content;
-	// var specificUser = req.user;
+	var title = req.body.title;
+	var topic = req.body.topic;
+	var url = req.body.url;
+	var content = req.body.content;
+	var specificUser = req.user._id;
 
-	var title = req.body.props.title;
-	var topic = req.body.props.topic;
-	var url = req.body.props.url;
-	var content = req.body.props.content;
-	var specificUser = req.user;
+	// var title = req.body.props.title;
+	// var topic = req.body.props.topic;
+	// var url = req.body.props.url;
+	// var content = req.body.props.content;
+	// // var specificUser = req.user;
+	// var specificUser = req.user._id;
 
 	var bucketList = new BucketList({
 		title: title,
@@ -26,4 +28,17 @@ exports.addBucketList = function(req, res, next){
 		if(err) { return next(err); }
 		res.json(bucketList);
 	});
+}
+
+exports.fetchBucketLists = function(req, res) {
+	var specificUser = req.user._id;
+	BucketList.find({specificUser: specificUser})
+	.then(
+		function fetchSuccess(data) {
+			res.json(data);
+		},
+		function fetchError(err) {
+			res.send(500, err.message);
+		}
+	);
 }
--- router.js
+++ router.js
@@ -1,20 +1,21 @@
-const Auth = require('./controllers/auth');
+var Auth = require('./controllers/auth');
 var BucketList = require('./controllers/bucketlistcontroller');
 
-const passportService = require('./services/passport');
-const passport = require('passport');
+var passportService = require('./services/passport');
+var passport = require('passport');
 
-let requireAuth = passport.authenticate('jwt', {session: false});
+var requireAuth = passport.authenticate('jwt', {session: false});
 var requireSignin = passport.authenticate('local', {session: false});
 
 module.exports = function(app){
 
-	app.get('/', requireAuth, function(req, res){
-		res.send({message: 'hey'});
-		//res.send({hi: 'there'});
-	});
-
 	app.post('/signup', Auth.signup);
 	app.post('/signin', requireSignin, Auth.signin);
 	app.post('/newitem', requireAuth, BucketList.addBucketList);
+	app.get('/items', requireAuth, BucketList.fetchBucketLists);
 }
+	// the below was in the module.exports function
+	// app.get('/', requireAuth, function(req, res){
+	// 	res.send({message: 'hey'});
+	// 	//res.send({hi: 'there'});
+	// });
